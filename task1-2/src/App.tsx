7324aafea1efedfe61fde7a3c4cbe0df4bd35274
takamonatsu <takataka521monatsu7191820@gmail.com>
2024-08-07 15:00:50 +0900

    課題2: カウンターアプリ

---
+++ task1-2/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import CounterApp from './CounterApp';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hollow, World!</h1>
+      <div className='counterApp'>
+        <CounterApp />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ task1-2/src/CounterApp.tsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+const CounterApp: React.FC  = () => {
+    const [count, setCount] = useState<number>(0);
+    const increment = () => {
+        setCount(count + 1);
+    }
+    const decrement = () => {
+        setCount(count - 1);
+    }
+  return (
+    <div>
+      <p>Counter: {count}</p>
+      <button onClick={increment} name='increment'>+</button>
+      <button onClick={decrement} name='decrement'>-</button>
+    </div>
+  )
+}
+
+export default CounterApp
