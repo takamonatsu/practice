add6e23f28ff927a6267f3a0f4ec7d4bb009c5ee
takamonatsu <takataka521monatsu7191820@gmail.com>
2024-08-14 21:19:24 +0900

    ログイン画面レイアウト作成

--- task8/src/App.tsx
+++ task8/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
 import './style/App.css';
+import './style/reset.css';
+import Login from './components/Longin/Login';
 
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
