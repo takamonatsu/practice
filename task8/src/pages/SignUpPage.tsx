804542e45dd2c4543ae32974316185604ceef22c
takamonatsu <takataka521monatsu7191820@gmail.com>
2024-08-17 13:26:55 +0900

    homeレイアウト作成 各画面レイアウト修正

--- task8/src/pages/SignUpPage.tsx
+++ task8/src/pages/SignUpPage.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import SignUp from '../components/UserFrom/UserFrom';
+import Header from '../components/Header/Header';
 
 const SignUpPage: React.FC = () => {
     return (
-        <div className='user-form-container'>
-            <div className='form-area'>
-                <h2 className='form-ttl'>SignUp</h2>
-                <SignUp userFrom='signup' />
+        <div>
+            <Header />
+            <div className='user-form-container'>
+                <div className='form-area'>
+                    <h2 className='form-ttl'>SignUp</h2>
+                    <SignUp userFrom='signup' />
+                </div>
             </div>
         </div>
     )
