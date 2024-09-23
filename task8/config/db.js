acc2e785e797829752a9e5dc12fa0be0735a896b
takamonatsu <takataka521monatsu7191820@gmail.com>
2024-09-23 18:29:37 +0900

    ファイル追加

---
+++ task8/config/db.js
@@ -0,0 +1,9 @@
+// youtute
+// https://www.youtube.com/watch?v=JBCykXLfvv0
+// https://github.com/Shin-sibainu/mongoDB-tutorial
+
+// mongodb+srv://takataka521monatsu7191820:ZPjEDQz5BYSI8HQL@cluster0.xyc0e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+// .connect
+// .then(() => console.log('データベース接続に成功しました'))
+// .catch((err) => console.log(err));
